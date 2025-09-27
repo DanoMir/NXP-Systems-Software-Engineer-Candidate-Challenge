9b4cf12a9cedbec040c8e017331f79b6ea2a5eff
danomiranda-admin <danielrmirandacortes@gmail.com>
2025-09-26 20:57:57 -0600

    Creation of simtemp.c structure.

---
+++ simtemp/kernel/nxp_simtemp.c
@@ -0,0 +1,20 @@
+/***************************************************************************
+* Open Source License 2025 NXP Semiconductor Challenge Stage
+****************************************************************************
+* Title        : nxp_simptemp.c
+*
+* Description  : Source File of simtemp
+*
+* Environment  : C Language
+*
+* Responsible  : Daniel R Miranda [danielrmirandacortes@gmail.com]
+*
+* Guidelines   : 
+*
+*
+* 
+*
+***************************************************************************/
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * Header Files *   *   *   *   *   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */
