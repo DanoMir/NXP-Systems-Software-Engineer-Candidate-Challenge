43f68f823ec2eb09e9d12156d8183626a861ad63
danomiranda-admin <danielrmirandacortes@gmail.com>
2025-10-22 11:58:03 -0600

    Creation of nxp_simtemp.h

---
+++ simtemp/kernel/nxp_simtemp.h
@@ -0,0 +1,24 @@
+/***************************************************************************
+        Open Source License 2025 NXP Semiconductor Challenge Stage
+****************************************************************************
+* Title        : nxp_simtemp.h
+* Description  : Header File of nxp_simtemp.c
+*
+* Environment  : C Language
+*
+* Responsible  : Daniel R Miranda [danielrmirandacortes@gmail.com]
+*
+* Guidelines   : Linux Kernel Coding Style
+*
+*
+* 
+*
+****************************************************************************
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+* * * * * * * * * * * Header Files *   *   *   *   *   *   *   *   *   *   *
+* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#ifndef _NXP_SIMTEMP_H_
+#define _NXP_SIMTEMP_H_
+
+#endif // End of _NXP_SIMTEMP_H_
