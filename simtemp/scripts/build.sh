4cc93b75fa041714dcdfde7e0478902b30d07d8f
danomiranda-admin <danielrmirandacortes@gmail.com>
2025-10-22 14:52:05 -0600

    Changes in run_demo.sh

--- simtemp/scripts/build.sh
+++ simtemp/scripts/build.sh
@@ -20,24 +20,24 @@ fail()
 }
 
 # --- 1. VERIFICATION AND SETUP ENVIRONMENT ---
-echo "----- 1. Checking environment and Kernel Headers ----"
+echo "----- 1. Checking environment and Kernel Headers -----"
 
 if [ ! -d "$KERNEL_DIR" ]; then
     fail "Kernel Headers not found for $KERNEL_VERSION. Please install with: 'sudo apt install linux-headers-$KERNEL_VERSION'"
 fi
 
 # --- 2. COMPILATION AND CLEANING KERNEL (C) ---
-echo "-----2. Compiling Kernel Module: ${MODULE_NAME}.ko ----"
+echo "----- 2. Compiling Kernel Module: ${MODULE_NAME}.ko ----"
 
 # Cleaning: Ensures that the Make clean uses the absolute path to Kernel directory
-echo "--- Executing make clean ---"
+echo "------- Executing make clean... --------"
 make -C "$KERNEL_DIR" M="$KERNEL_MODULE_PATH" clean || fail "Kernel Module Clean Failed"
 
 # Compilation: Construct the module
 make -C "$KERNEL_DIR" M="$KERNEL_MODULE_PATH" modules CFLAGS_MODULE="-Werror" || fail "Compilation Failed of Kernel Module"
 
 # --- 3. CONFIGURATION OF PYTHON ENVIRONMENT (User Space) ---
-echo "-----3. Configuring Python Virtual Environment (User Space)"
+echo "----- 3. Configuring Python Virtual Environment (User Space) -----"
 
 VENV_NAME="nxp_env"
 LOCAL_VENV_PATH="/tmp/$VENV_NAME"
@@ -61,7 +61,7 @@ REQUIREMENTS_FILE="$CLI_SOURCE_PATH/requirements.txt"
 #-----------Option with Virtual Machine----------------------
 # 2. Creation of VENV in a secure directory (/tmp)
 if [ ! -d "$LOCAL_VENV_PATH" ]; then
-    echo "   -> Creating venv in $LOCAL_VENV_PATH (Outside the Shared Folder of Virtual Machine)..."
+    echo " ------- Creating venv in $LOCAL_VENV_PATH (Outside the Shared Folder of Virtual Machine)...  ----------"
     # IMPORTANT: Not using sudo, since /tmp is a local directory
     python3 -m venv "$LOCAL_VENV_PATH" || fail "Creation of Virtual Environment: Failed."
 fi
@@ -76,7 +76,7 @@ fi
 PYTHON_VENV_PIP="$LOCAL_VENV_PATH/bin/pip"
 
 if [ -f "$REQUIREMENTS_FILE" ]; then
-    echo " Installing Python Dependencies from requirements.txt"
+    echo "-------- Installing Python Dependencies from requirements.txt --------"
     # Calling to pip of virtual environment (through of python binary)
     # and using sudo because venv was created by root.
     #sudo "$PYTHON_VENV_EXEC" -m pip install -r "$REQUIREMENTS_FILE" || fail "Installation of Python Dependencies Failed"
@@ -88,4 +88,4 @@ fi
 
 cd - > /dev/null
 
-echo "----- SUCCESS: Compilation and Setup Completed"
+echo "----- SUCCESS: Compilation and Setup Completed -------------"
